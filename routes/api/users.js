42e0aebfdbdfb5e99909b3af1cd94f88b945e7c0
nandan-shah <nandan2583@gmail.com>
2021-07-02 18:55:13 +0530

    user model,validation and user registration added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,65 @@
 const express= require('express');
 const router=express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt= require('bcryptjs');
+const gravatar = require('gravatar');
+const User=require('../../models/User');
 
-// @route     GET  api/users  (request type and endpoint)
+// @route     post  api/users  (request type and endpoint)
 // desc         Test route 
 // @access  public (means we do not need token for access it is public)
-router.get('/',(req,res) => {
-    res.send("users route");
+
+router.post('/', 
+
+// name must be an email
+[check('name',"name is required").not().notEmpty(),
+
+// email must be valid
+check('email','enter valid emai').isEmail(),
+
+// password must be at least 5 chars long
+check('password',"password must be at least 5 chars long").isLength({ min: 5 })],
+async (req,res) => {
+
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {name,email,password}=req.body;
+
+    try{
+    //see if user exist
+        let user=await User.findOne({email});
+        if(user){
+            return res.status(400).json({errors:[{msg:'user already exist'}]});
+        }
+    //get user gravatar
+        const avatar=gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        });
+
+        user=new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+    //encrypt password
+        const salt=await bcrypt.genSalt(10);
+
+        user.password=await bcrypt.hash(password, salt);
+        await user.save();
+        res.send('user register');
+    //return jsonwebtoken(bcz when user register we want to log in) 
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
 });
 
 module.exports =router;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 //connect database
 connectDB();
 
+//Init Middleware
+app.use(express.json({extended: false}));
+
+
 app.get('/' ,(req,res)=>{
 res.send("start");
 });
