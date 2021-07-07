c7f809f20cfa58499355b1c9e33a7e47a1482dcd
nandan-shah <nandan2583@gmail.com>
2021-07-07 16:20:03 +0530

    get all user profile added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,122 @@
 const express= require('express');
+const auth= require('../../middleware/auth');
 const router=express.Router();
+const { check, validationResult } = require('express-validator'); 
 
-// @route     GET  api/profile  (request type and endpoint)
-// desc         Test route 
-// @access  public (means we do not need token for access it is public)
-router.get('/',(req,res) => {
-    res.send("profile route");
+const Profile= require('../../models/Profile');
+const User= require('../../models/User');
+
+// @route     GET  api/profile/me  (request type and endpoint)
+// desc         get current user profile
+// @access  private (means we do need token for access it is public)
+
+router.get('/me',auth,async (req,res) => {
+   try {
+        const profile = await Profile.findOne({ user:req.user.id }).populate('user',['name','avatar']);
+        if(!profile){
+            return res.status(400).json({msg:"there is no profile for user"});
+        }
+        res.json(profile);
+
+   }catch (err) {
+       console.error(err);
+       res.status(500).send('server error');
+   }
 });
+
+
+// @route     POST  api/profile  (request type and endpoint)
+// desc         add and update user profile
+// @access  private (means we do need token for access it is public)
+router.post('/',[auth,[
+check('status','status required').not().isEmpty(),
+check('skills','skills require').not().isEmpty()
+]],async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    }=req.body;
+
+    //Build profile objectId
+    const profileFields ={};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+    //  console.log(profileFields.social);
+    try {
+    
+        let profile = await Profile.findOne({ user: req.user.id });
+        if(profile){
+            //update
+            profile=await Profile.findOneAndUpdate(
+                { user: req.user.id },{$set: profileFields},
+                { new: true});
+            return  res.json(profile);
+        }
+            //create a new
+            profile = new Profile(profileFields);
+            await profile.save();
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+      const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+      res.json(profiles);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
+// @route     POST  api/profile/:user_id  (request type and endpoint)
+// desc         view user profile
+// @access  public (means we do not need token for access it is public)
+
+router.get('/user/:user_id',async (req,res)=>{
+    try{
+            const profile= await Profile.findOne({_id:req.params.user_id}).populate('user',['name','avatar']);
+            res.json(profile);
+    }catch(err){
+        console.log(err.message);
+    }
+});
+
+
 
 module.exports =router;
